c8acd0fcac129a4f8b7bdc4d53428c797ebf782d
Aure0 <69534691+Aure0@users.noreply.github.com>
2023-01-10 22:28:04 +0100

    Update main.py
    TTS now works.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import revChatGPT
 import TTS
 from revChatGPT.ChatGPT import Chatbot
+import subprocess
+import os
+
 
 client = commands.Bot(command_prefix='-', intents=discord.Intents.all())
 chatbot = Chatbot({ "session_token": "CHATGPT_SESSION_TOKEN" })
@@ -15,10 +18,16 @@ async def ask(ctx, *, prompt):
   message = response["message"]
   message_parts = [message[i:i+1900] for i in range(0, len(message), 1900)]
   # TTS not working yet
-  if "tts_en" in ctx.message.content:
-    audio_file = TTS.synthesize(text=message, voice="en")
-    file = discord.File(audio_file, filename="message.wav")
-    await ctx.send(file=file)
+  if "--tts_en" in ctx.message.content:
+    tts_message = message
+    subprocess.run(["tts", "--text", tts_message, "--out_path", "output/output_tts_en.wav"])
+    while not os.path.exists("output/output_tts_en.wav"):
+      pass
+    with open("output/output_tts_en.wav", "rb") as f:
+      await ctx.send(file=discord.File(f))
+    for i, part in enumerate(message_parts):
+      await ctx.send(part)
+    await writing_message.delete()
   else:
     for i, part in enumerate(message_parts):
       await ctx.send(part)
